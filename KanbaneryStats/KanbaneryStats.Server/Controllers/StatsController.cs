d6b254b92dc57fc5ae5b8db8f3af4dbbccfc1819
James Sinclair <james.sinclair@biomni.com>
2018-06-16 14:58:47 +0100

    Initial solution

---
+++ KanbaneryStats/KanbaneryStats.Server/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using KanbaneryStats.Shared;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace KanbaneryStats.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatsController : Controller
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public StatsController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<TaskInfo> TaskInfo()
+        {
+            var contentRoot = hostingEnvironment.ContentRootPath;
+            var json = System.IO.File.ReadAllText($"{contentRoot}/data/taskinfo.json");
+            var taskInfos = JsonConvert.DeserializeObject<List<TaskInfo>>(json);
+         
+            return taskInfos;
+        }
+    }
+}
---
+++ KanbaneryStats/KanbaneryStats.Shared/TaskInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanbaneryStats.Shared
+{
+    public class TaskInfo
+    {
+        public string Title { get; set; }
+        public double Estimate { get; set; }
+        public double Actual { get; set; }
+        public double Factor => Actual/Estimate;
+    }
+}
