b66fb18a273d7627a9503183de5b48ea5498dd56
JamesSinclairBiomni <james.sinclair@biomni.com>
2018-07-13 19:06:24 +0100

    added individual factors to chart

--- KanbaneryStats/KanbaneryStats.Client/wwwroot/script/charts.js
+++ KanbaneryStats/KanbaneryStats.Client/wwwroot/script/charts.js
@@ -1,13 +1,15 @@
 ﻿Blazor.registerFunction('drawChart', (dataItems) => {
 
+    console.log(dataItems);
+
     google.charts.load('current', {
         'packages': ['line']
     });
 
     google.charts.setOnLoadCallback(() => {
 
-        var result = dataItems.map(a => [new Date(a.movedAt), a.averageFactor]);
-        result.splice(0, 0, ['Date', 'Average Velocity']);
+        var result = dataItems.map(a => [new Date(a.movedAt), a.factor, a.averageFactor]);
+        result.splice(0, 0, ['Date', 'Individual Factors', 'Average Factors']);
         var data = google.visualization.arrayToDataTable(result, false);
 
         var options = {
@@ -16,8 +18,8 @@
                 subtitle: 'graph of the average velocity over time'
             },
             curveType: 'function',
-            width: 900,
-            height: 500
+            width: 1000,
+            height: 700
         };
 
         var chart = new google.charts.Line(document.getElementById('linechart_material'));
